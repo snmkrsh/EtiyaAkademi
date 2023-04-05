a85e3ae68c4c25c8f7a36046506e7a4f17aa6208
snmkrsh <80041030+snmkrsh@users.noreply.github.com>
2023-04-05 16:24:02 +0300

    Create ders1.py

---
+++ ders1.py
@@ -0,0 +1,54 @@
+print("Hello")
+# yorum satırı
+# değişkenler :
+
+text = "Merhaba, Hoşgeldin !"
+print(text)
+print("Etiyali"+" "+text)
+
+text = "Selam"
+print(text)
+print(text)
+
+studentCount = 45 # integer tutar
+studentCount = "45" # string tutar
+
+averangePoint = 25.5 # double, decimal, float, gibi ondalıklı sayılar için
+print(averangePoint + 5)
+isVerified = True # bool, boolean; True veya False
+print(isVerified)
+
+# type ile veri tipini tutabiliriz ve print ile gösteririz.
+print(type(averangePoint))
+print(type(text))
+print(type(studentCount))
+print(type(isVerified))
+
+# Operatörler
+number = 10
+# matematiksel operatörler - mantıksal operatörler
+print(number + number)
+print(10 + 15)
+
+print(number - 5)
+print(17- 5)
+
+print(number / 2) # float döndürür
+print(10/2) # bölme işlemleri sonucu her zaman tam olamayacağı için float döner.
+
+print(number * 5)
+print(5 * 5)
+
+print(number % 2) #mod alma operatörü
+
+# mantıksal operatörler - karşılaştırma operatörleri
+print(number == 10) # true dönecek
+print(number == 11) # false dönecek
+print(number != 10) # false dönecek
+
+print(number < 10) # false
+print(number <= 10) # true
+
+print(number > 10) # false
+print(number >= 10) # true
+
