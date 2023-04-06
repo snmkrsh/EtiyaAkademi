4fdbcbc71eab72c326b524d7ba3e2df445a39469
snmkrsh <80041030+snmkrsh@users.noreply.github.com>
2023-04-06 09:21:27 +0300

    Update Ders1_Workshop.py

--- Ders1_Workshop.py
+++ Ders1_Workshop.py
@@ -1,5 +1,5 @@
 isimler = []  #isimler adında bos dizi olusturduk.
 for i in range(10):
     isim = input("İsminizi Giriniz: ") #kullanıcıdan veri istedik.
-    isimler.append(isim) #aldığımız veriyi diziye ekledik append komutu bunun için kullanıldı.
+    isimler.append(isim) #aldığımız veriyi diziye ekledik append() komutu bunun için kullanıldı, listenin sonuna yeni ögeyi ekler.
 print(isimler)
