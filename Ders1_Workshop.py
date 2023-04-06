3b5fd8be5c963c436827413c7b0d18335dbce7a9
snmkrsh <80041030+snmkrsh@users.noreply.github.com>
2023-04-06 09:18:19 +0300

    Create Ders1_Workshop.py

---
+++ Ders1_Workshop.py
@@ -0,0 +1,5 @@
+isimler = []  #isimler adında bos dizi olusturduk.
+for i in range(10):
+    isim = input("İsminizi Giriniz: ") #kullanıcıdan veri istedik.
+    isimler.append(isim) #aldığımız veriyi diziye ekledik append komutu bunun için kullanıldı.
+print(isimler)
